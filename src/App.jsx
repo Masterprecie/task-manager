b15f6f4f9c43731c94fefaeaa7fd19c2d8a77305
Precious <ikpaprecious2@gmail.com>
2023-09-10 22:08:40 +0100

    ch:added reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import EmptyState from './components/EmptyState';
+import taskReducer from './taskReducer';
+
+const initialState = [];
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  // const [tasks, setTasks] = useState([]);
 
-  const addTask = (newTask) => {
-    setTasks([...tasks, { ...newTask, id: Date.now() }]);
+  // const addTask = (newTask) => {
+  //   setTasks([...tasks, { ...newTask, id: Date.now() }]);
+  // };
+
+  // const deleteTask = (taskId) => {
+  //   setTasks(tasks.filter(task => task.id !== taskId));
+  // };
+
+  const [tasks, dispatch] = useReducer(taskReducer, initialState);
+  const [nextTaskId, setNextTaskId] = useState(1);
+
+  const addTask = (task) => {
+    const newTask = { ...task, id: nextTaskId };
+    dispatch({ type: 'ADD_TASK', payload: newTask });
+    setNextTaskId(nextTaskId + 1);
   };
 
   const deleteTask = (taskId) => {
-    setTasks(tasks.filter(task => task.id !== taskId));
+    dispatch({ type: 'DELETE_TASK', payload: taskId });
   };
 
   return (
     <div className='container'>
       <AddTask onAdd={addTask} />
-
 
       {tasks.length > 0 ? (
         <TaskList tasks={tasks} onDelete={deleteTask} />
---
+++ src/taskReducer.js
@@ -0,0 +1,13 @@
+
+const taskReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_TASK':
+			return [...state, action.payload];
+		case 'DELETE_TASK':
+			return state.filter(task => task.id !== action.payload);
+		default:
+			return state;
+	}
+};
+
+export default taskReducer;
